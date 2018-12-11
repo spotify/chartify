Fix the axis precision when the max value equals to the min value

chartify/_core/plot.py
@@ -36,7 +36,8 @@ class BasePlot:
     @staticmethod
     def _axis_format_precision(max_value, min_value):
         difference = abs(max_value - min_value)
-        precision = abs(int(np.floor(np.log10(difference)))) + 1
+        precision = abs(int(np.floor(
+            np.log10(difference if difference else 1)))) + 1
         zeros = ''.join(['0']*precision)
         return "0,0.[{}]".format(zeros)
 