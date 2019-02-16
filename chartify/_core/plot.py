Fixed precision error; upgrade ipython; added second axis example

chartify/_core/plot.py
@@ -37,7 +37,11 @@ class BasePlot:
     @staticmethod
     def _axis_format_precision(max_value, min_value):
         difference = abs(max_value - min_value)
-        precision = abs(int(np.floor(np.log10(difference)))) + 1
+        if difference:
+            precision = abs(int(np.floor(np.log10(difference)))) + 1
+        else:
+            precision = 1
+        print(precision)
         zeros = ''.join(['0']*precision)
         return "0,0.[{}]".format(zeros)
 